cb7951e3aeb3bfe3feea8a7cb8719ddffe779cf7
krfurlong <krfurlong@users.noreply.github.com>
2023-11-12 17:04:04 +0000

    deploy: cf614aaaf76ff234c28b9cc749720aeb2d847942

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -10,7 +10,7 @@ var CONFIGURED_HTML_URL_PREFIX = "/artyrix-campaign";
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '223012541229297234791187257786522843811'                       // used to check whether the localStorage data is stale
+var gzip_hash = '315123553387309595160080424491344432820'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
