ae027682ab3323ea60cc8f3ee86f49cf9948f514
syracard <syracard@users.noreply.github.com>
2024-06-27 19:37:45 +0000

    deploy: 49caa4caf54d9348d5f2755fbd50fd90476f56a8

--- obs.html/static/obsidian_core.js
+++ obs.html/static/obsidian_core.js
@@ -13,7 +13,7 @@ var CONFIG_CLOSE_LEFT_PANE_IF_EMPTY = 0;
 var RELATIVE_PATHS = 0;
 var documentation_mode = 1;
 var tab_mode = !no_tab_mode;
-var gzip_hash = '136076781157567431221340338505946083904'                       // used to check whether the localStorage data is stale
+var gzip_hash = '56272167146584126278401152756115991001'                       // used to check whether the localStorage data is stale
 
 // global cache
 var fn_cache_ls_available = null;
